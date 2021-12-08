74b5f19d930aa260e68bcaf5c2948c96af5f17e7
Wes Beard <wesley.beard@mymail.champlain.edu>
2021-12-07 19:18:26 -0500

    Initial design

---
+++ app/src/main/java/com/example/iotclock/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.iotclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.*
+import java.util.*
+
+const val SERVER_URI = "tcp://broker.hivemq.com:1883"
+const val CLIENT_ID = "Android Client"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var client: MqttAndroidClient
+    private lateinit var reconnect: Button
+    private lateinit var displaySpinner: Spinner
+    private lateinit var clockSpinner: Spinner
+    private lateinit var setClockButton: Button
+    private lateinit var setAlarmButton: Button
+
+    companion object {
+        var customTime = false
+        var setClock = true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        client = MqttAndroidClient(this, SERVER_URI, CLIENT_ID)
+        connect()
+
+        reconnect = findViewById(R.id.reconnect)
+        reconnect.setOnClickListener {
+            connect()
+        }
+
+        displaySpinner = findViewById(R.id.display_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.display_mode_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            displaySpinner.adapter = adapter
+        }
+        displaySpinner.onItemSelectedListener = DisplaySpinner()
+
+        clockSpinner = findViewById(R.id.clock_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.clock_set_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            clockSpinner.adapter = adapter
+        }
+        clockSpinner.onItemSelectedListener = ClockSpinner()
+
+        setClockButton = findViewById(R.id.set_clock_button)
+        setClockButton.setOnClickListener {
+            setClock = true
+            if (customTime) {
+                TimePickerFragment().show(supportFragmentManager, "timePicker")
+            }
+            else {
+                publish("t/time", DateFormat.format("HH:mm:ss", Date()).toString())
+            }
+        }
+
+        setAlarmButton = findViewById(R.id.set_alarm)
+        setAlarmButton.setOnClickListener {
+            setClock = false
+            TimePickerFragment().show(supportFragmentManager, "timePicker")
+        }
+    }
+
+    private fun connect() {
+        client = MqttAndroidClient(this, SERVER_URI, CLIENT_ID)
+
+        client.setCallback(object : MqttCallback {
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                Log.d(CLIENT_ID, "Receive message: ${message.toString()} from topic: $topic")
+            }
+
+            override fun connectionLost(cause: Throwable?) {
+                Log.d(CLIENT_ID, "Connection lost ${cause.toString()}")
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                Log.d(CLIENT_ID, "Delivery complete to $SERVER_URI")
+            }
+        })
+
+        val options = MqttConnectOptions()
+        try {
+            client.connect(options, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(CLIENT_ID, "Connected!")
+                    makeToast("Connected!")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(CLIENT_ID, "Connection failed")
+                    makeToast("Connection failed")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    fun publish(topic: String, data: String) {
+        try {
+            val message = MqttMessage(data.toByteArray())
+            client.publish(topic, message)
+            makeToast("Message sent!")
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    class ClockSpinner: AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            customTime = position != 0
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // No action needs to be taken
+        }
+    }
+
+    class DisplaySpinner: AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            // Toast.makeText(view?.context, "Display Item Selected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // No action needs to be taken
+        }
+    }
+
+    fun makeToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/example/iotclock/TimePickerFragment.kt
@@ -0,0 +1,42 @@
+package com.example.iotclock
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.text.format.DateFormat
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Use the current time as the default values for the picker
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        // Create a new instance of TimePickerDialog and return it
+        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+    }
+
+    override fun onTimeSet(view: TimePicker, hour: Int, minute: Int) {
+        var hourFmt = hour.toString()
+        var minuteFmt = minute.toString()
+        if (minute < 10)
+        {
+            minuteFmt = "0$minuteFmt"
+        }
+        if (hour < 10)
+        {
+            hourFmt = "0$hourFmt"
+        }
+
+        if (MainActivity.setClock) {
+            (activity as MainActivity)?.publish("t/time", "$hourFmt:$minuteFmt:00")
+        }
+        else {
+            (activity as MainActivity)?.publish("t/alarm", "$hourFmt:$minuteFmt:00")
+        }
+    }
+}
